d10acb4d765b2b39b1e4279a49df67f0c1bdf609
artyom-zyktin <zyktin.a.a@gmail.com>
2021-01-09 15:40:12 +0400

    Lets begin.

---
+++ Example.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace DimsQ
+{
+    interface IServer
+    { }
+    interface IClient
+    { }
+
+    class Foo
+    {
+        IServer server;
+        IClient client;
+
+        Queue messages;
+    }
+
+}
